75f56af5dfd6eb256a3dfc92296c084e401c9c53
dospro <dospro@gmail.com>
2020-06-02 22:57:09 -0500

    Added circleExample to node js implementation

--- src/node_main.ts
+++ src/node_main.ts
@@ -1,7 +1,11 @@
 import * as fs from 'fs';
 
-import {color, mult, normalize, point, sum, Tuple, vector} from "./features/tuples";
+import {color, mult, normalize, point, sub, sum, Tuple, vector} from "./features/tuples";
 import {canvas, canvas_to_ppm, write_pixel} from "./features/canvas";
+import {intersect, sphere} from "./features/spheres";
+import {translation} from "./features/transformations";
+import {ray} from "./features/rays";
+import {hit} from "./features/interesctions";
 
 interface Projectile {
     position: Tuple;
@@ -20,7 +24,7 @@ function tick(env: Environment, proj: Projectile): Projectile {
     };
 }
 
-function main() {
+function projectileExample() {
     let p: Projectile = {
         position: point(0, 1, 0),
         velocity: mult(normalize(vector(1, 1.8, 0)), 11.25),
@@ -46,7 +50,48 @@ function main() {
         }
         console.log('Done');
     });
+}
 
+function circleExample() {
+    let c = canvas(800, 800);
+    const col = color(1, 0, 0);
+
+    const shape = sphere();
+    shape.transform = translation(0, 0, 0);
+
+    const ray_origin = point(0, 0, -5);
+    const wall_z = 10;
+    const wall_size = 7.0;
+
+    const pixel_size = wall_size / 800;
+    const half = wall_size / 2;
+
+    for (let y = 0; y < 800; y++) {
+        const world_y = half - pixel_size * y;
+        for (let x = 0; x < 800; x++) {
+            const world_x = -half + pixel_size * x;
+            const position = point(world_x, world_y, wall_z);
+            const r = ray(ray_origin, normalize(sub(position, ray_origin)));
+            const xs = intersect(shape, r);
+
+            if (hit(xs)) {
+                write_pixel(c, x, y, col);
+            }
+        }
+    }
+
+    const text = canvas_to_ppm(c);
+    fs.writeFile('circleExample.ppm', text, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Done');
+    });
+}
+
+function main() {
+    circleExample();
 }
 
 main();
